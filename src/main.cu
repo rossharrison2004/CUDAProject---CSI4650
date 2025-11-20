8a25d9b248c2350abcdbce758f9f193ceface49f
Ross <rharrison@oakland.edu>
2025-11-20 15:44:17 -0500

    fixed opencv issue

--- src/main.cu
+++ src/main.cu
@@ -1,6 +1,9 @@
 #include "cpu_baseline.cpp"
 #include "harris_v1.cu"
-#include <opencv2/opencv.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 #include <iostream>
 
 int main() {
